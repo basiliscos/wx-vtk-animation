0c3a2e43b06f9a9f3589a4a21bc54d12c5369b88
Ivan Baidakou <the.dmol@yandex.by>
2025-08-08 14:55:24 +0300

    Add tabs

--- src/App.cpp
+++ src/App.cpp
@@ -10,19 +10,18 @@
 #include <vtkRenderer.h>
 #include <vtkSphereSource.h>
 
-#include <wx/wx.h>
-#include <wx/slider.h>
 #include <wx/evtloop.h>
+#include <wx/slider.h>
+#include <wx/wx.h>
+
+#include <wx/listbook.h>
+#include <wx/notebook.h>
 
 #if !defined(__WXMSW__)
-#include <gtk/gtk.h>
 #include <gdk/gdk.h>
 #include <gdk/gdkx.h>
+#include <gtk/gtk.h>
 #endif
-
-#include <bit>
-
-// adopted from https://examples.vtk.org/site/Cxx/Utilities/AnimationScene/
 
 /*****************************************************************************/
 
@@ -30,16 +29,15 @@ namespace utils {
 
 // there is a need to get native window id to let it be OpenGL'ized by vtk
 
-void* NativeHandle(wxWindow* window)
-{
-	auto handle = window->GetHandle();
+void *NativeHandle(wxWindow *window) {
+  auto handle = window->GetHandle();
 #if defined(__WXMSW__)
-	return reinterpret_cast<void*>(handle);
+  return reinterpret_cast<void *>(handle);
 #elif defined(__WXGTK20__)
-	gtk_widget_realize(handle); // otherwise xwindow id will can be null
-	//gtk_widget_set_double_buffered(handle, false);
-	GdkWindow* gdk = gtk_widget_get_window(GTK_WIDGET(handle));
-	return reinterpret_cast<void*>(GDK_WINDOW_XID(gdk));
+  gtk_widget_realize(handle); // otherwise xwindow id will can be null
+  // gtk_widget_set_double_buffered(handle, false);
+  GdkWindow *gdk = gtk_widget_get_window(GTK_WIDGET(handle));
+  return reinterpret_cast<void *>(GDK_WINDOW_XID(gdk));
 #else
 #error "TODO: add platform support"
 #endif
@@ -47,7 +45,7 @@ void* NativeHandle(wxWindow* window)
 
 /*****************************************************************************/
 
-} // namespace
+} // namespace utils
 
 /*****************************************************************************/
 
@@ -55,224 +53,12 @@ void* NativeHandle(wxWindow* window)
 
 /*****************************************************************************/
 
-wxDEFINE_EVENT(ANIMATION_FINISHED, wxCommandEvent);
-wxDEFINE_EVENT(ANIMATION_TICK, wxCommandEvent);
-
-/*****************************************************************************/
-
-class CueAnimator
-{
+class MyApp : public wxApp {
 public:
+  using Parent = wxApp;
 
-/*****************************************************************************/
-
-  CueAnimator(wxWindow* window)
-  {
-	this->SphereSource = 0;
-	this->Mapper = 0;
-	this->Actor = 0;
-	this->widget = window;
-  }
-
-/*****************************************************************************/
-
-  ~CueAnimator()
-  {
-	this->Cleanup();
-  }
-
-/*****************************************************************************/
-
-  void StartCue(vtkAnimationCue::AnimationCueInfo* vtkNotUsed(info),
-				vtkRenderer* ren)
-  {
-	vtkNew<vtkNamedColors> colors;
-
-	this->SphereSource = vtkSphereSource::New();
-	this->SphereSource->SetRadius(0.5);
-
-	this->Mapper = vtkPolyDataMapper::New();
-	this->Mapper->SetInputConnection(this->SphereSource->GetOutputPort());
-
-	this->Actor = vtkActor::New();
-	this->Actor->SetMapper(this->Mapper);
-	this->Actor->GetProperty()->SetSpecular(0.6);
-	this->Actor->GetProperty()->SetSpecularPower(30);
-	this->Actor->GetProperty()->SetColor(
-		colors->GetColor3d("Peacock").GetData());
-
-	ren->AddActor(this->Actor);
-	ren->ResetCamera();
-	ren->Render();
-
-	// process wx events
-	WxTick();
-  }
-
-/*****************************************************************************/
-
-  void Tick(vtkAnimationCue::AnimationCueInfo* info, vtkRenderer* ren)
-  {
-	double newradius = 0.1 +
-		(static_cast<double>(info->AnimationTime - info->StartTime) /
-		 static_cast<double>(info->EndTime - info->StartTime)) *
-			1;
-	this->SphereSource->SetRadius(newradius);
-	this->SphereSource->Update();
-	ren->Render();
-
-	// notify percentage for wx
-	wxCommandEvent event(ANIMATION_TICK, widget->GetId());
-	double percent = static_cast<long>(100 * info->AnimationTime / (info->EndTime - info->StartTime));
-	event.SetExtraLong(percent);
-	widget->ProcessWindowEvent(event);
-
-	// process wx events
-	WxTick();
-  }
-
-/*****************************************************************************/
-
-  void EndCue(vtkAnimationCue::AnimationCueInfo* vtkNotUsed(info),
-			  vtkRenderer* ren)
-  {
-	(void)ren;
-	this->Cleanup();
-
-	// notify finishing
-	wxCommandEvent event(ANIMATION_FINISHED, widget->GetId());
-	widget->ProcessWindowEvent(event);
-
-	// process wx events
-	WxTick();
-  }
-
-/*****************************************************************************/
-
-protected:
-
-/*****************************************************************************/
-
-  vtkSphereSource* SphereSource;
-  vtkPolyDataMapper* Mapper;
-  vtkActor* Actor;
-  wxWindow* widget;
-
-  // inner loop for allowing wx events to be processed during animation
-  // this is needed because wx event loop is NOT REENTERABLE while
-  // VTK animation is synchronous
-  wxGUIEventLoop loop;
-
-/*****************************************************************************/
-
-  void Cleanup()
-  {
-	if (this->SphereSource != 0)
-	{
-	  this->SphereSource->Delete();
-	  this->SphereSource = 0;
-	}
-
-	if (this->Mapper != 0)
-	{
-	  this->Mapper->Delete();
-	  this->Mapper = 0;
-	}
-	if (this->Actor != 0)
-	{
-	  this->Actor->Delete();
-	  this->Actor = 0;
-	}
-  }
-
-/*****************************************************************************/
-
-  void WxTick()
-  {
-	  // activate inner event loop
-	  wxEventLoopActivator activate(&loop);
-	  while (loop.Pending())
-	  {
-		  loop.Dispatch();
-	  }
-  }
-
-/*****************************************************************************/
-
-};
-
-/*****************************************************************************/
-
-class vtkAnimationCueObserver : public vtkCommand
-{
-public:
-
-/*****************************************************************************/
-
-  static vtkAnimationCueObserver* New()
-  {
-	return new vtkAnimationCueObserver;
-  }
-
-/*****************************************************************************/
-
-  virtual void Execute(vtkObject* vtkNotUsed(caller), unsigned long event,
-					   void* calldata)
-  {
-	if (this->Animator != 0 && this->Renderer != 0)
-	{
-	  vtkAnimationCue::AnimationCueInfo* info =
-		  static_cast<vtkAnimationCue::AnimationCueInfo*>(calldata);
-	  switch (event)
-	  {
-	  case vtkCommand::StartAnimationCueEvent:
-		this->Animator->StartCue(info, this->Renderer);
-		break;
-	  case vtkCommand::EndAnimationCueEvent:
-		this->Animator->EndCue(info, this->Renderer);
-		break;
-	  case vtkCommand::AnimationCueTickEvent:
-		this->Animator->Tick(info, this->Renderer);
-		//wxApp().ProcessPendingEvents();
-		break;
-	  }
-	}
-	if (this->RenWin != 0)
-	{
-	  this->RenWin->Render();
-	}
-  }
-
-/*****************************************************************************/
-
-  vtkRenderer* Renderer;
-  vtkRenderWindow* RenWin;
-  CueAnimator* Animator;
-
-protected:
-
-/*****************************************************************************/
-
-  vtkAnimationCueObserver()
-  {
-	this->Renderer = 0;
-	this->Animator = 0;
-	this->RenWin = 0;
-  }
-
-/*****************************************************************************/
-
-};
-
-/*****************************************************************************/
-
-class MyApp: public wxApp
-{
-public:
-	using Parent = wxApp;
-
-	bool OnInit() override;
-	int OnExit() override;
+  bool OnInit() override;
+  int OnExit() override;
 };
 
 /*****************************************************************************/
@@ -281,262 +67,212 @@ struct MyPanel;
 
 /*****************************************************************************/
 
-class MyFrame: public wxFrame
-{
+class MyFrame : public wxFrame {
 public:
-	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-	void OnClick(wxCommandEvent&);
-	void OnFinish(wxCommandEvent&);
-	void OnTick(wxCommandEvent&);
+  MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
 
 private:
-	wxSlider* slider;
-	wxButton* button;
-	MyPanel* panel;
-	vtkSmartPointer<vtkAnimationScene> scene;
+  wxSlider *slider;
+  wxButton *button;
+  MyPanel *panel;
+  vtkSmartPointer<vtkAnimationScene> scene;
 };
 
 /*****************************************************************************/
 
-struct MyInteractor: vtkRenderWindowInteractor
-{
-	using Parent = vtkRenderWindowInteractor;
+struct MyInteractor : vtkRenderWindowInteractor {
+  using Parent = vtkRenderWindowInteractor;
 
-	MyInteractor(wxPanel* panel);
-	~MyInteractor();
-	void Initialize() override;
-	void UpdateSize(int x, int y) override;
+  MyInteractor(wxPanel *panel);
+  ~MyInteractor();
+  void Initialize() override;
+  void UpdateSize(int x, int y) override;
 
-	private:
-	wxPanel* panel; // wx widget to be OpenGL'ized
+private:
+  wxPanel *panel; // wx widget to be OpenGL'ized
 };
 
 /*****************************************************************************/
 
 // Proxies wx-events to VTK events
-struct MyPanel: public wxPanel
-{
-	using Parent = wxPanel;
+struct MyPanel : public wxPanel {
+  using Parent = wxPanel;
 
-	MyPanel(wxWindow* parent);
-	~MyPanel();
+  MyPanel(wxWindow *parent, const char *sphere_color);
+  ~MyPanel();
 
-	void OnRender(wxPaintEvent& event);
-	void OnResize(wxSizeEvent& evt);
+  void OnRender(wxPaintEvent &event);
+  void OnResize(wxSizeEvent &evt);
+  void OnTabChange(wxBookCtrlEvent &);
 
-	MyInteractor* interactor;
-	vtkRenderer* renderer;
+  MyInteractor *interactor;
+  vtkRenderer *renderer;
 
-	DECLARE_EVENT_TABLE()
+  DECLARE_EVENT_TABLE()
 };
 
 /*****************************************************************************/
 
 BEGIN_EVENT_TABLE(MyPanel, wxPanel)
-	EVT_PAINT(MyPanel::OnRender)
-	EVT_SIZE(MyPanel::OnResize)
+EVT_PAINT(MyPanel::OnRender)
+EVT_SIZE(MyPanel::OnResize)
 END_EVENT_TABLE()
 
 /*****************************************************************************/
 
-bool MyApp::OnInit()
-{
-	DEBUG_MESSAGE("%s", "MyApp::OnInit");
+bool MyApp::OnInit() {
+  DEBUG_MESSAGE("%s", "MyApp::OnInit");
 
-	MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
-	frame->Show( true );
-	return true;
+  MyFrame *frame =
+      new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+  frame->Show(true);
+  return true;
 }
 
 /*****************************************************************************/
 
-int MyApp::OnExit()
-{
-	DEBUG_MESSAGE("%s", "MyApp::OnExit");
-	return Parent::OnExit();
+int MyApp::OnExit() {
+  DEBUG_MESSAGE("%s", "MyApp::OnExit");
+  return Parent::OnExit();
 }
 
 /*****************************************************************************/
 
-MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-		: wxFrame(NULL, wxID_ANY, title, pos, size)
-{
-	auto sizer = new wxBoxSizer(wxVERTICAL);
-	slider = new wxSlider(this, wxID_ANY,  0, 0, 100);
-	button = new wxButton(this, wxID_ANY, "play");
-	panel = new MyPanel(this);
-	panel->SetBackgroundColour(*wxGREEN);
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size) {
+  auto sizer = new wxBoxSizer(wxVERTICAL);
+  auto tabs = new wxListbook(this, wxID_HIGHEST + 1, wxDefaultPosition,
+                             wxDefaultSize, wxNB_TOP);
 
-	sizer->Add(slider, 0, wxEXPAND | wxALL, 3);
-	sizer->Add(button, 0, wxEXPAND | wxALL, 3);
-	sizer->Add(panel, 1, wxEXPAND | wxALL, 3);
+  auto page_0 = new MyPanel(tabs, "Green");
+  auto page_1 = new MyPanel(tabs, "Red");
+  auto page_2 = new MyPanel(tabs, "Cyan");
 
-	SetSizerAndFit(sizer);
+  tabs->AddPage(page_0, _("page_0"), true);
+  tabs->AddPage(page_1, _("page_1"), false);
+  tabs->AddPage(page_2, _("page_2"), false);
 
-	SetSize(wxSize(640, 480));
+  sizer->Add(tabs, 1, wxEXPAND);
 
-	button->Bind(wxEVT_BUTTON, &MyFrame::OnClick, this);
-	Bind(ANIMATION_FINISHED, &MyFrame::OnFinish, this);
-	Bind(ANIMATION_TICK, &MyFrame::OnTick, this);
-
-	scene = vtkSmartPointer<vtkAnimationScene>::New();
+  SetSizerAndFit(sizer);
+  PostSizeEvent();
 }
 
 /*****************************************************************************/
 
-void MyFrame::OnClick(wxCommandEvent&)
-{
-	DEBUG_MESSAGE("%s", "MyFrame::OnClick, enter");
-	if (!scene->IsInPlay())
-	{
+MyPanel::MyPanel(wxWindow *parent, const char *sphere_color)
+    : Parent(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+             wxFULL_REPAINT_ON_RESIZE) {
+  SetBackgroundStyle(wxBG_STYLE_CUSTOM);
 
-		button->SetLabel("Stop");
+  interactor = new MyInteractor(this);
+  renderer = vtkRenderer::New();
+  interactor->GetRenderWindow()->AddRenderer(renderer);
 
-		scene->SetModeToRealTime();
+  renderer->SetBackground(0, 0, 0);
+  renderer->ResetCamera();
 
-		scene->SetLoop(0);
-		scene->SetFrameRate(5);
-		scene->SetStartTime(0);
-		scene->SetEndTime(3);
+  vtkNew<vtkSphereSource> sphereSource;
+  sphereSource->SetCenter(0.0, 0.0, 0.0);
+  sphereSource->SetRadius(5.0);
 
-		// Create an Animation Cue.
-		vtkNew<vtkAnimationCue> cue1;
-		cue1->SetStartTime(0);
-		cue1->SetEndTime(3);
-		scene->AddCue(cue1);
+  // Make the surface smooth:
+  sphereSource->SetPhiResolution(100);
+  sphereSource->SetThetaResolution(100);
 
-		// Create cue animator;
-		CueAnimator animator(this);
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(sphereSource->GetOutputPort());
 
-		// Create Cue observer.
-		vtkNew<vtkAnimationCueObserver> observer;
-		observer->Renderer = panel->renderer;
-		observer->Animator = &animator;
-		observer->RenWin = panel->interactor->GetRenderWindow();
-		//observer->event_loop = GetEventHandler();
+  vtkNew<vtkNamedColors> colors;
 
-		cue1->AddObserver(vtkCommand::StartAnimationCueEvent, observer);
-		cue1->AddObserver(vtkCommand::EndAnimationCueEvent, observer);
-		cue1->AddObserver(vtkCommand::AnimationCueTickEvent, observer);
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetColor(colors->GetColor3d(sphere_color).GetData());
 
-		//scene->SetModeToSequence();
-		DEBUG_MESSAGE("%s", "MyFrame::OnClick, play");
-		scene->Play();
+  renderer->AddActor(actor);
+  interactor->GetRenderWindow()->Render();
+  interactor->Start();
 
-		DEBUG_MESSAGE("%s", "MyFrame::OnClick, stop");
-		scene->Stop();
-
-		panel->interactor->Start();
-	}
-	else
-	{
-		scene->Stop();
-	}
-	DEBUG_MESSAGE("%s", "MyFrame::OnClick, leave");
+  parent->Bind(wxEVT_NOTEBOOK_PAGE_CHANGED, &MyPanel::OnTabChange, this);
+  parent->Bind(wxEVT_LISTBOOK_PAGE_CHANGED, &MyPanel::OnTabChange, this);
 }
 
 /*****************************************************************************/
 
-void MyFrame::OnFinish(wxCommandEvent&)
-{
-	DEBUG_MESSAGE("%s", "MyFrame::OnFinish");
-	button->SetLabel("Play");
-	panel->renderer->Clear();
+MyPanel::~MyPanel() {
+  DEBUG_MESSAGE("%s", "MyPanel::~MyPanel");
+  renderer->Delete();
+  interactor->GetRenderWindow()->Delete();
+  interactor->Delete();
 }
 
 /*****************************************************************************/
 
-void MyFrame::OnTick(wxCommandEvent& event)
-{
-	DEBUG_MESSAGE("%s", "MyFrame::OnTick");
-	slider->SetValue(static_cast<int>(event.GetExtraLong()));
+void MyPanel::OnRender(wxPaintEvent &event) {
+  DEBUG_MESSAGE("%s", "MyPanel::OnRender");
+  interactor->GetRenderWindow()->Render();
 }
 
 /*****************************************************************************/
 
-MyPanel::MyPanel(wxWindow* parent): Parent(parent)
-{
-	interactor = new MyInteractor(this);
-	interactor->SetRenderWindow(vtkRenderWindow::New());
-	renderer = vtkRenderer::New();
-	interactor->GetRenderWindow()->AddRenderer(renderer);
-
-	renderer->SetBackground(219 / 255.0, 222 / 255.0, 248 / 255.0);
-	renderer->ResetCamera();
+void MyPanel::OnResize(wxSizeEvent &evt) {
+  auto w = evt.GetSize().GetWidth();
+  auto h = evt.GetSize().GetHeight();
+  DEBUG_MESSAGE("MyPanel::OnResize %d x %d", w, h);
+  interactor->UpdateSize(w, h);
+  Refresh();
 }
 
 /*****************************************************************************/
 
-MyPanel::~MyPanel()
-{
-	DEBUG_MESSAGE("%s", "MyPanel::~MyPanel");
-	renderer->Delete();
-	interactor->GetRenderWindow()->Delete();
-	interactor->Delete();
+void MyPanel::OnTabChange(wxBookCtrlEvent &event) {
+  DEBUG_MESSAGE("%s %p", "MyFrame::OnTabChange", this);
+  // interactor->GetRenderWindow()->Initialize();
+  // interactor->Initialize();
+  // interactor->GetRenderWindow()->WindowRemap();
 }
 
 /*****************************************************************************/
 
-void MyPanel::OnRender(wxPaintEvent& event)
-{
-	wxPaintDC pDC(this);
-//	interactor->Render();
-	interactor->GetRenderWindow()->Render();
+MyInteractor::MyInteractor(wxPanel *panel_) : panel{panel_} {
+  this->RenderWindow = NULL;
+  this->SetRenderWindow(vtkRenderWindow::New());
+  // this->RenderWindow->Delete();
+  DEBUG_MESSAGE("%s", "MyInteractor::MyInteractor()");
 }
 
 /*****************************************************************************/
 
-void MyPanel::OnResize(wxSizeEvent& evt)
-{
-	auto w = evt.GetSize().GetWidth();
-	auto h = evt.GetSize().GetHeight();
-	interactor->UpdateSize(w, h);
+MyInteractor::~MyInteractor() {
+  DEBUG_MESSAGE("%s", "MyInteractor::~MyInteractor");
 }
 
 /*****************************************************************************/
 
-MyInteractor::MyInteractor(wxPanel* panel_): panel{panel_}
-{
-	DEBUG_MESSAGE("%s", "MyInteractor::MyInteractor()");
+void MyInteractor::Initialize() {
+  DEBUG_MESSAGE("%s", "MyInteractor::Initialize()");
+  Parent::Initialize();
+  RenderWindow->SetWindowId(utils::NativeHandle(panel));
+  RenderWindow->SetParentId(utils::NativeHandle(panel->GetParent()));
+  RenderWindow->SetDisplayId(RenderWindow->GetGenericDisplayId());
+
+  int *size = RenderWindow->GetSize();
+  DEBUG_MESSAGE("MyInteractor::Initialize(), sz = %u x %u", size[0], size[1]);
+
+  Size[0] = size[0];
+  Size[1] = size[1];
 }
 
 /*****************************************************************************/
 
-MyInteractor::~MyInteractor()
-{
-	DEBUG_MESSAGE("%s", "MyInteractor::~MyInteractor");
-}
-
-/*****************************************************************************/
-
-void MyInteractor::Initialize()
-{
-	DEBUG_MESSAGE("%s", "MyInteractor::Initialize()");
-	Parent::Initialize();
-	auto handle = utils::NativeHandle(panel);
-	RenderWindow->SetWindowId(reinterpret_cast<void *>(handle));
-	RenderWindow->SetParentId(utils::NativeHandle(panel->GetParent()));
-	RenderWindow->SetDisplayId(RenderWindow->GetGenericDisplayId());
-
-	int *size = RenderWindow->GetSize();
-	DEBUG_MESSAGE("MyInteractor::Initialize(), sz = %u x %u", size[0], size[1]);
-
-	Size[0] = size[0];
-	Size[1] = size[1];
-}
-
-/*****************************************************************************/
-
-void MyInteractor::UpdateSize(int w, int h)
-{
-	if (w != Size[0] || h != Size[1])
-	{
-		DEBUG_MESSAGE("MyInteractor::UpdateSize(), sz = %u x %u", w, h);
-		Size[0] = w;
-		Size[1] = h;
-		RenderWindow->SetSize(w, h);
-		panel->Refresh();
-	}
+void MyInteractor::UpdateSize(int w, int h) {
+  if (w != Size[0] || h != Size[1]) {
+    DEBUG_MESSAGE("MyInteractor::UpdateSize(), sz = %u x %u", w, h);
+    Size[0] = w;
+    Size[1] = h;
+    RenderWindow->SetSize(w, h);
+    panel->Refresh();
+  }
 }
 
 /*****************************************************************************/
